10ce0a181897f01c903e2f8ed78a2ee92e660a2e
programer1983 <programer972@gmail.com>
2022-03-08 16:28:07 +0200

    API mapping and displaying names;

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,24 @@
 import Searchbar from "../components/Searchbar"
+import {useContext} from "react"
+import ShowsContext from "./../context/shows/showsContext"
 
 const Homepage = () => {
+  const showsContext = useContext(ShowsContext)
+  const {loading, shows} = showsContext
+
+
   return (
     <div className="homepage">
         <Searchbar />
+      {loading ? (
+          <h2>Loading...</h2>
+        ) : (
+         <div>
+           {shows.map((item) => (
+             <h3>{item.show.name}</h3>
+           ))}
+         </div>
+      )}
     </div>
   )
 }
