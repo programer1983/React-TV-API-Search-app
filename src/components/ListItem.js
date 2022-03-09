b821f81c6afbe122cfe588d5feb90c6f61759c65
programer1983 <programer972@gmail.com>
2022-03-09 10:12:33 +0200

    movie list output, movie card creation, stylization;

---
+++ src/components/ListItem.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const ListItem = ({image, name, rating, id}) => {
+
+  return (
+    <div>
+        <Link to={`/singleshow/${id}`} className="listitem">
+            <img src={image} alt={name} />
+            <div className="listitem__info">
+                <h4 className="info__name">{name}</h4>
+                <h4 className="info__rating">{rating}</h4>
+            </div>
+        </Link>
+    </div>
+  )
+}
+
+export default ListItem
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import Searchbar from "../components/Searchbar"
 import {useContext} from "react"
 import ShowsContext from "./../context/shows/showsContext"
+import ListItem from "../components/ListItem"
 
 const Homepage = () => {
   const showsContext = useContext(ShowsContext)
@@ -13,9 +14,19 @@ const Homepage = () => {
       {loading ? (
           <h2>Loading...</h2>
         ) : (
-         <div>
+         <div className="homepage__list">
            {shows.map((item) => (
-             <h3>{item.show.name}</h3>
+             <ListItem
+                key={item.show.id}
+                id={item.show.id}
+                image={
+                  item.show.image 
+                  ? item.show.image.medium 
+                  : "https://www.publicdomainpictures.net/pictures/30000/velka/film-background-1334067869u9d.jpg"
+                } 
+                name={item.show.name} 
+                rating={item.show.rating.average ? item.show.rating.average : "No rating"} 
+              />
            ))}
          </div>
       )}
