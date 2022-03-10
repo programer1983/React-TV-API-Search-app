ce07d6e6d85b726fe4b7f5a8adf6cb00b75a9510
programer1983 <programer972@gmail.com>
2022-03-10 14:34:28 +0200

    create  about  page  and  styles;

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -2,7 +2,24 @@
 const Aboutpage = () => {
   return (
     <div className="aboutpage">
-        Aboutpage
+        <div className="about">
+          <div className="about__info">
+          <h3 className="about__title">ABOUT PAGE</h3>
+            <p>Tv Show Search App (v1.0)</p>
+            <div>
+              <p>Things learnt from app build:</p>
+              <ul>
+                <li>React (create-react-app)</li>
+                <li>Basic CSS & HTML</li>
+                <li>Axios AJAX Request</li>
+                <li>TVMAZE API (https://api.tvmaze.com/api)</li>
+                <li>React Router</li>
+                <li>React Context API</li>
+                <li>Deploing to Netify</li>
+              </ul>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
